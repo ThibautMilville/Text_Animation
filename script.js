00f74f832cc47c234a8d43542a1ba9cad3c2ffeb
ThibautMilville <tmilville.pro@gmail.com>
2023-03-19 12:41:52 +0100

    changement_text

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const textLoad = () => {
     setTimeout(() => {
         text.textContent = "Freelancer"
     },0);
+    setTimeout(() => {
+        text.textContent = "Developper"
+    },4000);
+    setTimeout(() => {
+        text.textContent = "Creator"
+    },8000);
 }
 
 textLoad()
